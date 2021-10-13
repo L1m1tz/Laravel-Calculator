ede9a49726e5205493c3d3fccb5cb08c111efe58
L1m1tz <andreew.mk@gmail.com>
2021-10-14 01:52:55 +0200

    created a new controller for calculations

---
+++ app/Http/Controllers/CalculatorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalculatorController extends Controller
+{
+    //
+    public function calculatefunc(Request $request){
+
+        echo "route sucessfully connects view and controller";
+
+    }
+}
